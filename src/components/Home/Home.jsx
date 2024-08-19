5905c569b30067e19917a93d88d09ca8392a380c
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-08-19 19:28:49 +0200

    Feat: modified layout

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Devices from "../Devices/Devices"
+import "./Home.css"
+
+const Home = () => {
+  return (
+    <div className="home-design">
+      <div className="home-content">
+        <Devices />
+      </div>
+    </div>
+  )
+}
+
+export default Home
